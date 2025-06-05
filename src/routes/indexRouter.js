e92b8dbcbb424de6c02651bd600931ef11c64281
Damucho <damis96@hotmail.com.ar>
2025-06-04 21:49:59 -0300

    (feat) added link detail

--- src/routes/indexRouter.js
+++ src/routes/indexRouter.js
@@ -2,24 +2,10 @@ const express = require("express");
 const router = express.Router();
 let db = require('../../data/models');
 
+
 //MAIN ROUTE
 router.get('/', function(req, res){
     res.render("index");
-});
-
-
-//ADD ROUTE
-router.get('/add', function(req, res){
-    res.render("addLink");
-});
-
-router.post('/add', async function(req, res){
-    const newLink = await db.Link.create({
-        title: req.body.title,
-        url: req.body.link,
-        description: req.body.description
-    });
-    res.redirect('/');
 });
 
 
@@ -27,10 +13,33 @@ router.post('/add', async function(req, res){
 router.get('/links', async function(req, res){
     try {
         const links = await db.Link.findAll();
-        res.render("links", {links: links});
+        res.render("links", {link: links});
     } catch (error) {
         console.log(error);
     }
+});
+
+
+//LINK ID ROUTE
+router.get('/links/:id', async function(req, res){
+    let link = await db.Link.findByPk(req.params.id);
+    console.log(link);
+    res.render('linkDetail', {link: link});
+})
+
+
+//ADD ROUTE
+router.get('/links/add', function(req, res){
+    res.render("addLink");
+});
+
+router.post('/links/add', async function(req, res){
+    const newLink = await db.Link.create({
+        title: req.body.title,
+        url: req.body.link,
+        description: req.body.description
+    });
+    res.redirect('/links');
 });
 
 
